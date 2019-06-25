ac098bb3094add80ee6c670ffe0ad0c580e7bee8
unknown <guevara.torres619@outlook.com>
2019-06-24 23:09:32 -0400

    First Commit

---
+++ app/src/main/java/com/example/powerreceiver/CustomReceiver.java
@@ -0,0 +1,38 @@
+package com.example.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import static android.content.Intent.ACTION_HEADSET_PLUG;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+        if (intentAction != null) {
+            String toastMessage = "unknown intent action";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Power connected!";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power disconnected!";
+                    break;
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = "Custom Broaadcast Recieved";
+                    break;
+                case ACTION_HEADSET_PLUG:
+                    toastMessage = "Headset plug!";
+
+            }
+
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
